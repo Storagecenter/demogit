bfda129be0b9946be9dfba4daa97fbec2c3d0652
Storagecenter <naveenchandru95@gmail.com>
2021-06-19 23:43:27 +0530

    This is the change in testgit

--- src/constructor/Keythis.java
+++ src/constructor/Keythis.java
@@ -5,7 +5,7 @@ public class Keythis {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Thiskey kd = new Thiskey();
-kd.key(10, 11, 13);
+//kd.key(10, 11, 13);
 System.out.println(+kd.adm());
 	}
 
