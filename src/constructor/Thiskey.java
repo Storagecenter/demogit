1a0f2b64caaf55cd2e10005ca44439599241b491
Storagecenter <naveenchandru95@gmail.com>
2021-06-19 22:57:39 +0530

    Second comit by adding file

--- src/constructor/Thiskey.java
+++ src/constructor/Thiskey.java
@@ -6,17 +6,26 @@ public class Thiskey {
 		int ab;
 		int bc;
 		int cd;
+		int ef;
 		
-	public void key(int ab, int bc,int cd) {
+	public void key(int ab, int bc,int cd,int ef) {
 		
 		this.ab=ab;
 		this.bc=bc;
 		this.cd=cd;
+		this.ef=ef;
 	}
 
 	public int adm(){
 		return ab;
 	}
 	
+	public int cm(){
+		return ab;
+	}
+	
+	public int dmm(){
+		return ab;
+	}
 
 }
